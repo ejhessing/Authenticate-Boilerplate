b35b70062aa2a68ab6e9f8d4e0c374a6e49c67b5
Erwin Hessing <ejhessing@Erwins-MacBook-Pro.local>
2016-10-18 18:16:02 +1300

    start server with passport

---
+++ index.js
@@ -0,0 +1,32 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+const LocalStrategy = require('passport-local').Strategy;
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + "/views");
+
+app.use(sessions({ secret: 'whatever' }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.get("/", (req,res) => {
+  res.render('index')
+})
+
+app.listen(PORT, () => console.log('Listening on port: ' + PORT));
